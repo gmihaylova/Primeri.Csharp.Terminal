0e1da9d33abff3fb16876e3b6eac29f695bb2c00
Гергана Михайлова <geri_rai@abv.bg>
2016-10-13 23:59:22 +0300

    Модул 02, Видео 04 - Работа със стрингове

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -12,8 +12,15 @@ namespace Modul02
 			//Събиране на числа
 			a = 10;
 			b = 5;
+			string program_name = "Modul02 - Примери";
+			string program_version = "1.0";
 
 			sum = a + b;
+
+			//Данни за програмата
+			Console.WriteLine ( program_name );
+			Console.WriteLine ( "Версия: " + program_version + "\n\n" );
+
 			//Писане в конзола
 			Console.Write ( a );
 			Console.Write (" + ");
@@ -46,8 +53,11 @@ namespace Modul02
 			a++; 
 			Console.WriteLine ( a );
 
-			Console.WriteLine ("Стандартно делене: " + ( 7 / 4 ).ToString ()); // -> 2 ( 7 / 3 = 2,3333 )
-			Console.WriteLine ("Остатък от делене: " + ( 7 % 4 ).ToString ()); // -> 1 ( 1/3 = 0,3333 )
+			Console.WriteLine ("Стандартно делене: " + ( 7 / 3 ).ToString ()); // -> 2 ( 7 / 3 = 2,3333 )
+			Console.WriteLine ("Остатък от делене: " + ( 7 % 3 ).ToString ()); // -> 1 ( 1/3 = 0,3333 )
+
+
+
 		}
 	}
 }
