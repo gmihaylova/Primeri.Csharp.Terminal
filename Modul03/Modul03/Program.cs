7f5afe49d7c3cd88dbdb5a72258b54bcf92f04cb
Гергана Михайлова <geri_rai@abv.bg>
2016-11-21 23:00:22 +0200

    Модул 03, Упражнение - първо постижение на Гери

--- Modul03/Modul03/Program.cs
+++ Modul03/Modul03/Program.cs
@@ -9,9 +9,10 @@ namespace Modul03
 			double a = 0, b = 0, c = 0;
 			Console.Write (">a = "); a = Convert.ToDouble( Console.ReadLine () );
 			Console.Write (">b = "); b = Convert.ToDouble( Console.ReadLine () );
+			Console.Write (">c = "); c = Convert.ToDouble( Console.ReadLine () );
 
 			string _format = "#0.00 m3";
-			Console.WriteLine ("\n\n>a / b = " + (a / b).ToString( _format ));
+			Console.WriteLine ("\n\n>a * b * c = " + (a * b * c).ToString( _format ));
 
 		}
 	}
